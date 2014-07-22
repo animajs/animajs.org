977889523a4cc37ff76c6f1f103b9de079162e44
SMbey0nd <wanyan.wz@taobao.com>
2014-07-22 18:58:50 +0800

    remove src

--- src/js/doc-jump.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  
-  $(document).delegate('.doc', 'click', function() {
-    $("#pt-main").css('display', 'none');
-    $("#iframe").css('display', 'block');
-  });
-});
